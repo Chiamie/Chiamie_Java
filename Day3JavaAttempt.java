c374971b704ed8b662374d5a4b2a4a5d592f2114
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-07 02:17:41 +0100

    day3 question attempt in java

---
+++ Day3JavaAttempt.java
@@ -0,0 +1,49 @@
+//import java.util.Arrays;
+
+public class Day3JavaAttempt {
+public static void main(String[] args){
+
+
+splitThis("I love programming");
+}
+
+public static void splitThis(String sentence){
+	String[] words = sentence.split(" ");
+	System.out.print("result = ");
+	for(String word : words){
+		System.out.print(word + ", ");
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
