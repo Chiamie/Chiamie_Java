8a09acd535b0582b1bc470c7f9605109ff5d2e48
Chiamie <okechukwurchiamaka@gmail.com>
2025-10-12 03:44:14 +0100

    done with exercise 8.4

--- Self/Dietel/ChapterEight/Test/TestCylinder.java
+++ Self/Dietel/ChapterEight/Test/TestCylinder.java
@@ -1,13 +1,41 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestCylinder {
     @Test
-    public void testThatRadiusIsSetToPositiveNumbers(){
+    public void testThatRadiusIsSetToPositiveNumber(){
         Cylinder cylinder = new Cylinder();
         cylinder.setRadius(5);
         assertEquals(5, cylinder.getRadius());
+    }
+
+    @Test
+    public void testThatRadiusIsSetToPositiveNumbersThrowsExceptionWithNegativeValues(){
+        Cylinder cylinder = new Cylinder();
+        assertThrows(IllegalArgumentException.class, () -> cylinder.setRadius(-5));
+    }
+
+    @Test
+    public void testThatHeightIsSetToPositiveNumber(){
+        Cylinder cylinder = new Cylinder();
+        cylinder.setHeight(5);
+        assertEquals(5, cylinder.getHeight());
+    }
+
+    @Test
+    public void testThatHeightIsSetToPositiveNumbersThrowsExceptionWithNegativeValues(){
+        Cylinder cylinder = new Cylinder();
+        assertThrows(IllegalArgumentException.class, () -> cylinder.setHeight(-5));
+    }
+
+    @Test
+    public void testThatAccurateVolumeOfCylinderIsReturned(){
+        Cylinder cylinder = new Cylinder();
+        cylinder.setHeight(5);
+        cylinder.setRadius(5);
+        assertEquals(392.75, cylinder.calculateVolume());
     }
 
 }
--- Self/Dietel/ChapterEight/src/Cylinder.java
+++ Self/Dietel/ChapterEight/src/Cylinder.java
@@ -7,10 +7,14 @@ public class Cylinder  {
         return 3.142 * radius * radius * height;
     }
     public void setRadius(double radius){
-        if (radius > 0) this.radius = radius;
+        if (radius < 0)
+            throw new IllegalArgumentException("Radius cannot be negative");
+        this.radius = radius;
     }
     public void setHeight(double height){
-        if (height > 0) this.height = height;
+        if (height < 0)
+            throw new IllegalArgumentException("Height cannot be negative");
+        this.height = height;
     }
     public double getRadius(){
         return radius;
