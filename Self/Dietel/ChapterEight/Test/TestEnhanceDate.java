d1f31c8f7c7cc26e1db067160ec7ac35fde5c414
Chiamie <okechukwurchiamaka@gmail.com>
2025-10-17 04:09:41 +0100

    EnhanceDate can validate day for specific month and year; EnhanceDate only accepts years with four digits

--- Self/Dietel/ChapterEight/Test/TestEnhanceDate.java
+++ Self/Dietel/ChapterEight/Test/TestEnhanceDate.java
@@ -6,10 +6,16 @@ public class TestEnhanceDate {
     @Test
     public void testThatTheEnhanceDateValidatesDayForASpecificMonth() {
         assertThrows(IllegalArgumentException.class, () -> new EnhanceDate(2024, 4, 31));
-
-
-
     }
 
+    @Test
+    public void testThatTheEnhanceDateValidatesDayForASpecificYearAndMonth() {
+        assertThrows(IllegalArgumentException.class, () -> new EnhanceDate(2023, 2, 29));
+    }
+
+    @Test
+    public void testThatTheEnhanceDateValidatesOnlyYearWith4Digits() {
+        assertThrows(IllegalArgumentException.class, () -> new EnhanceDate(20235, 2, 28));
+    }
 
 }
